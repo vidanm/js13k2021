6434820456685c9198146576796bee610a9ce1ca
vidanm <vidan.tauriff@gmail.com>
2021-08-26 15:56:12 +0200

    tout se passe niquel la

--- game.js
+++ game.js
@@ -29,23 +29,47 @@ class Sprite {
 	}
 }
 
+class Anchor {
+	constructor ({spr=null,color='white'}){
+		this.spr = spr;
+		let rand = Math.random()*3;
+		this.color = ( rand > 1) ? '#48A9A6' : '#D4B483';
+		this.color = ( rand > 2 ) ? '#C1666B' : this.color;
+	}
+}
+
 class Line {
-	constructor ({x=0, y=0, anchorX=0,anchorY=0,color='white',ctx=null}){
+	constructor ({x=0, y=0, anchorX=250,anchorY=700,color='white',ctx=null}){
 		this.x = x;
 		this.y = y;
-		this.anchorX = anchorX;
-		this.anchorY = anchorY;
 		this.color = color;
 		this.ctx = ctx;
+		let anchor = new Anchor({spr:new Sprite({x:anchorX, y:anchorY}),color:'white'});
+		this.anchors = [anchor];
+	}
+
+	addAnchor(anchor){
+		this.anchors.push(anchor);
 	}
 
 	render(){
-		this.ctx.strokeStyle = this.color;
-		this.ctx.lineWidth = 3;
-		this.ctx.beginPath();
-		this.ctx.moveTo(this.anchorX,this.anchorY);
-		this.ctx.lineTo(this.x,this.y);
-		this.ctx.stroke();
+		
+		this.ctx.lineWidth = 5;
+		for (var i = 0;i<this.anchors.length;i++){
+
+			this.ctx.beginPath();
+			if ( i < this.anchors.length -1 ){
+				this.ctx.moveTo(this.anchors[i].spr.x, this.anchors[i].spr.y);
+				this.ctx.lineTo(this.anchors[i+1].spr.x, this.anchors[i+1].spr.y);
+				this.ctx.strokeStyle = this.anchors[i].color;
+			}
+			else {
+				this.ctx.moveTo(this.anchors[i].spr.x, this.anchors[i].spr.y);
+				this.ctx.lineTo(this.x, this.y);
+				this.ctx.strokeStyle = this.color;
+			}
+			this.ctx.stroke();
+		}
 	}
 
 	update(){
@@ -54,47 +78,87 @@ class Line {
 	}
 }
 
-let pointerX = 0
-let pointerY = 0
+function distance(x1, y1, x2, y2){
+	return Math.sqrt(
+		Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2)
+	)
+}
+
+function isLineColliding(){
+	stars.forEach((element) => {
+	
+		let anlen = line.anchors.length;
+		let distab = distance(line.x, line.y, element.x, element.y);
+		let distbc = distance(element.x, element.y, line.anchors[anlen -1].spr.x, line.anchors[anlen-1].spr.y);
+		let distac = distance(line.x, line.y, line.anchors[anlen-1].spr.x, line.anchors[anlen-1].spr.y);
+
+		if ((distab + distbc) -distac <= 1){
+			let anchor = new Anchor({spr:element,color:'green'});
+			line.addAnchor(anchor);
+		}
+	});
+	return null;
+}
+
+let pointerX = 0;
+let pointerY = 0;
+
 let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-let bg = new Sprite({x:250,y:350,color:'#040626',width:500,height:500,ctx:ctx});
-let line = new Line({ctx:ctx});
 let canvas_width = 500;
 let canvas_height = 700;
+let ctx = canvas.getContext('2d');
 
+let bg = new Sprite({x:250,y:350,color:'#000220',width:500,height:500,ctx:ctx});
+let line = new Line({ctx:ctx});
+let stars = []
+for (var i = 0;i<30;i++){
+	let dy = Math.random()*1;
+	//let color = ( dy < 2 ) ? 'white' : 'darkblue';
+	let color = 'white';
+	let star = new Sprite({
+					x:Math.random()*canvas_width,
+					y:Math.random()*canvas_height,
+					dy:dy,
+					color:color,
+					width:5+3*Math.random(),
+					height:5+3*Math.random(),
+					ctx:ctx});
+	stars.push(star);
+}
 
-// DRAW THINGS ON CANVAS
+/* DRAW THINGS ON CANVAS */
 function draw(){
 	bg.render();
 	line.render();
+	stars.forEach((element) => element.render());
 }
 
-// CLEAR CANVAS
+/* CLEAR CANVAS */
 function clear(){
 	ctx.clearRect(0,0,canvas_width,canvas_height);
 }
 
-// UPDATE SPRITES AND OBJECTS
+/* UPDATE SPRITES AND OBJECTS*/
 function update(){
 	bg.update();
 	line.update();
+	stars.forEach((element) => element.update());
+	isLineColliding();
 }
 
-// TRACK MOUSE POSITION
+/* TRACK MOUSE POSITION */
 document.addEventListener('mousemove', (event) => {
-	    pointerX = event.clientX;
-		pointerY = event.clientY;
+		var rect = canvas.getBoundingClientRect();
+	    pointerX = event.clientX - rect.left;
+		pointerY = event.clientY - rect.top;
 });
 
-// MAIN FUNCTION
+/* MAIN FUNCTION */
 function main(evt){
 	setInterval(()=>{
 	clear();
 	update();
 	draw();
-	//choose next rectangle color before drawing
-	//recalls the code above every frame at a rate of 60 per second
 	},1000/60)
 }
 
