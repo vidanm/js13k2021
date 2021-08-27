0ef51b1f7b7a0e04bb8bd864d57809f45038f4ed
vidanm <vidan.tauriff@gmail.com>
2021-08-27 14:02:14 +0200

    designed logo

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ class Sprite {
 		this.ctx.beginPath();
 		this.ctx.fillStyle = this.color;
 		this.ctx.strokeStyle = this.color;
-		this.ctx.lineWidth = 3;
+		this.ctx.lineWidth = 5;
 		if (this.image != null)
 			this.ctx.drawImage(this.image,this.x,this.y);
 		else
@@ -38,6 +38,20 @@ class Anchor {
 	}
 }
 
+class Enemy {
+	constructor (spr){
+		this.spr = spr;
+	}
+
+	render(){
+		this.spr.render();
+	}
+
+	update(){
+		this.spr.update();
+	}
+}
+
 class Line {
 	constructor ({x=0, y=0, anchorX=250,anchorY=700,color='white',ctx=null}){
 		this.x = x;
@@ -53,10 +67,8 @@ class Line {
 	}
 
 	render(){
-		
 		this.ctx.lineWidth = 5;
 		for (var i = 0;i<this.anchors.length;i++){
-
 			this.ctx.beginPath();
 			if ( i < this.anchors.length -1 ){
 				this.ctx.moveTo(this.anchors[i].spr.x, this.anchors[i].spr.y);
@@ -93,11 +105,56 @@ function isLineColliding(){
 		let distac = distance(line.x, line.y, line.anchors[anlen-1].spr.x, line.anchors[anlen-1].spr.y);
 
 		if ((distab + distbc) -distac <= 1){
-			let anchor = new Anchor({spr:element,color:'green'});
+			let anchor = new Anchor({spr:element});
 			line.addAnchor(anchor);
 		}
 	});
 	return null;
+}
+
+function drawLogo(ctx){
+	/* '#48A9A6' : '#D4B483';
+		this.color = ( rand > 2 ) ? '#C1666B' : this.color; */
+	
+	ctx.beginPath();
+	ctx.lineWidth = 50;
+	ctx.strokeStyle= '#48A9A6';
+	ctx.arc(100, 350, 50, Math.PI*2, Math.PI+Math.PI/2);
+	ctx.stroke();
+	
+	ctx.beginPath();
+	ctx.strokeStyle= '#D4B483';
+	ctx.moveTo(100, 300);
+	ctx.lineTo(350, 300);
+	ctx.stroke();
+
+	ctx.moveTo(225, 300);
+	ctx.lineTo(225, 425);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.strokeStyle= '#C1666B';
+	ctx.moveTo(325,275);
+	ctx.lineTo(325,425);
+	ctx.stroke();
+
+	ctx.moveTo(325,400);
+	ctx.lineTo(425,400);
+	ctx.stroke();
+}
+
+function initStar(){
+	let dy = 2+Math.random()*1;
+	//let color = ( dy < 2 ) ? 'white' : 'darkblue';
+	let color = 'white';
+	return new Sprite({
+					x:Math.random()*canvas_width,
+					y:0-Math.random()*canvas_width,
+					dy:dy,
+					color:color,
+					width:5+3*Math.random(),
+					height:5+3*Math.random(),
+					ctx:ctx});
 }
 
 let pointerX = 0;
@@ -112,53 +169,67 @@ let bg = new Sprite({x:250,y:350,color:'#000220',width:500,height:500,ctx:ctx});
 let line = new Line({ctx:ctx});
 let stars = []
 for (var i = 0;i<30;i++){
-	let dy = Math.random()*1;
-	//let color = ( dy < 2 ) ? 'white' : 'darkblue';
-	let color = 'white';
-	let star = new Sprite({
-					x:Math.random()*canvas_width,
-					y:Math.random()*canvas_height,
-					dy:dy,
-					color:color,
-					width:5+3*Math.random(),
-					height:5+3*Math.random(),
-					ctx:ctx});
+	let star = initStar();
 	stars.push(star);
 }
 
-/* DRAW THINGS ON CANVAS */
+let enemies = []
+for (var i = 0; i< 20;i++){
+	let spr = new Sprite({
+		x:Math.random()*canvas_width,
+		y:Math.random()*canvas_height/4,
+		dx:2*(Math.random()-0.5),
+		dy:2*(Math.random()),
+		width:5,
+		height:5,
+		ctx:ctx,
+		color:'red'});
+	let enemy = new Enemy(spr);
+	enemies.push(enemy);
+}
+
 function draw(){
 	bg.render();
 	line.render();
 	stars.forEach((element) => element.render());
+	enemies.forEach((element) => element.render());
 }
 
-/* CLEAR CANVAS */
-function clear(){
-	ctx.clearRect(0,0,canvas_width,canvas_height);
-}
+function clear(){ctx.clearRect(0,0,canvas_width,canvas_height);}
 
-/* UPDATE SPRITES AND OBJECTS*/
 function update(){
 	bg.update();
 	line.update();
 	stars.forEach((element) => element.update());
+	enemies.forEach((element) => element.update());
 	isLineColliding();
+
+	for (var i=0;i<line.anchors.length;i++){
+		if (line.anchors[i].spr == null || line.anchors[i].spr.y > canvas_height*0.8){
+			line.anchors.splice(i,1);
+		}
+	}
+
+	for (var i=0;i<stars.length;i++){
+		if (stars[i].y > canvas_height*1.5){
+			let star = initStar();
+			stars.splice(i,1,star);
+		}
+	}
 }
 
-/* TRACK MOUSE POSITION */
 document.addEventListener('mousemove', (event) => {
 		var rect = canvas.getBoundingClientRect();
 	    pointerX = event.clientX - rect.left;
 		pointerY = event.clientY - rect.top;
 });
 
-/* MAIN FUNCTION */
 function main(evt){
 	setInterval(()=>{
 	clear();
 	update();
 	draw();
+	//drawLogo(ctx);
 	},1000/60)
 }
 
